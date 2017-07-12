324cba8d8448fc87d3725b40775384c087dd4f16
Victor Marin <marin250189@gmail.com>
2017-07-11 22:50:40 -0400

    change hooks names, put the new filters and shorcode in the integration class

--- classes/wc4bp_subscription_manager.php
+++ classes/wc4bp_subscription_manager.php
@@ -25,15 +25,6 @@ class wc4bp_subscription_manager {
 			//loading_dependency
 			require_once WC4BP_SUBSCRIPTION_CLASSES_PATH . 'wc4bp_subscription_integration.php';
 			new wc4bp_subscription_integration();
-			
-			add_action( 'admin_enqueue_scripts', array( $this, 'enqueue_js' ) );
-			add_action( 'admin_enqueue_scripts', array( $this, 'enqueue_style' ) );
-            add_filter( 'wc4bp_subscription_menu_items', array( $this, 'wc4bp_subscription_menu_items' ) );
-            add_filter( 'wc4bp_subscription_page', array( $this, 'subscription_page' ) );
-            add_filter( 'wc4bp_view_subscription_page', array( $this, 'view_subscription_page' ) );
-
-            add_shortcode( 'woo_subscriptions_page', array( $this, "wc4bp_my_account_process_shortcode_subscriptions_page" ) );
-            add_shortcode( 'woo_subscriptions_view_page', array( $this, "wc4bp_my_account_process_shortcode_subscriptions_view_page" ) );
 
 		} catch ( Exception $ex ) {
 			wc4bp_subscription_log::log( array(
@@ -44,71 +35,6 @@ class wc4bp_subscription_manager {
 			) );
 		}
 	}
-	
-	/**
-	 * Include styles in admin
-	 *
-	 * @param $hook
-	 * @param bool $force
-	 */
-
-	public function view_subscription_page($path){
-        $path = '../../'.WC4BP_SUBSCRIPTION_BASENAME.'/view/view-subscription';
-
-        return $path;
-
-    }
-    public function subscription_page( $path ) {
-
-        $path = '../../'.WC4BP_SUBSCRIPTION_BASENAME.'/view/subscription';
-
-        return $path;
-    }
-    public function wc4bp_my_account_process_shortcode_subscriptions_view_page( $attr, $content = "" ) {
-        wc_print_notices();
-        wc_get_template( 'myaccount/view-subscription.php', array(), '', plugin_dir_path( WC_Subscriptions::$plugin_file ) . 'templates/' );
-    }
-    public function  wc4bp_my_account_process_shortcode_subscriptions_page($attr, $content){
-        wc_print_notices();
-        wc_get_template( 'myaccount/subscriptions.php', array(), '', plugin_dir_path( WC_Subscriptions::$plugin_file ) . 'templates/' );
-
-    }
-    public function wc4bp_subscription_menu_items( $menu_items ) {
-
-        // Add our menu item after the Orders tab if it exists, otherwise just add it to the end
-        if ( array_key_exists( 'orders', $menu_items ) ) {
-            $menu_items = wcs_array_insert_after( 'orders', $menu_items, 'subscriptions', __( 'Subscriptions', 'woocommerce-subscriptions' ) );
-        } else {
-            $menu_items['subscriptions'] = __( 'Subscriptions', 'woocommerce-subscriptions' );
-        }
-
-        return $menu_items;
-    }
-	public static function enqueue_style( $hook, $force = false ) {
-		global $post;
-		if ( ( ( $hook == 'post.php' || $hook == 'post-new.php' ) && $post->post_type == 'product' ) || $force ) {
-			wp_enqueue_style( 'jquery' );
-			wp_enqueue_style( 'wc4bp-subscription', WC4BP_SUBSCRIPTION_CSS_PATH . 'wc4bp-subscription.css', array(), self::getVersion() );
-		}
-	}
-	
-	/**
-	 * Include script
-	 *
-	 * @param $hook
-	 * @param bool $force
-	 */
-	public static function enqueue_js( $hook, $force = false ) {
-		global $post;
-		if ( ( ( $hook == 'post.php' || $hook == 'post-new.php' ) && $post->post_type == 'product' ) || $force ) {
-			wp_register_script( 'wc4bp_subscription', WC4BP_SUBSCRIPTION_JS_PATH . 'wc4bp-subscription.js', array( "jquery" ), self::getVersion() );
-			wp_enqueue_script( 'wc4bp_subscription' );
-			wp_localize_script( 'wc4bp_subscription', 'wc4bp_subscription', array(
-				'ajax_url' => admin_url( 'admin-ajax.php' ),
-			) );
-		}
-	}
-	
 	/**
 	 * Get plugins version
 	 *
--- view/subscription.php
+++ view/subscription.php
@@ -10,7 +10,7 @@
 ?>
 <div id="item-body" role="main">
     <?php do_action( 'wc4bp_before_checkout_body' ); ?>
-<p>Hola Mundo</p>
+
     <?php echo do_shortcode( '[woo_subscriptions_page]' ); ?>
 
     <?php do_action( 'wc4bp_after_checkout_body' ); ?>
